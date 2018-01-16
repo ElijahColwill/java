7f93cae4f1624756a6faffbb2c71a4da440e7d6f
EljiahColwill <elijah.c035@gmail.com>
2018-01-16 11:19:11 -0700

    HELP ME

--- Switch.java
+++ Switch.java
@@ -12,9 +12,35 @@ class Switch {
 
 	public static String switchStrings(String str, String strSwitch1, String strSwitch2) {
 
-		String str1 = str.replace(strSwitch1, strSwitch2);
-		String str2 = str1.replace(strSwitch2, strSwitch1);
-		return str2;
+		String temporary = "TEMPORARY";
+
+		for (int i = 0; i < str.length(); i++){
+			search = str.substring(i, i+strSwitch1.length());
+			if (str.equal(search)) {
+				str = str(0, i) + temporary + str(i+strSwitch1.length());
+			}
+		}
+
+		System.out.println(str);
+		return str;
+
+
+		// loop through initial in segments of the length of strSwitch1
+		//		look for spots the same as strSwitch1
+		//		if i find one, replace the segment with a temporary swap string
+		
+		// loop through initial in segments of the length of strSwitch2
+		//		look for spots the same as strSwitch2
+		//		if i find one, replace the segment with strSwitch1
+		
+		// loop through initial in segments of the length of temporary
+		//		look for spots the same as temporary
+		//		if i find one, replace the segment with strSwitch1
+
+		// str = str.replace(strSwitch1, temporary);
+		// str = str.replace(strSwitch2, strSwitch1);
+		// str = str.replace(temporary, strSwitch2);
+		// return str;
 
 	}
 
